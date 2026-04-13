981c11fcecdbd7ab50fabb62ac5ae209ee0ebcb7
Soah Fernandes <soah.fernandes2005@gmail.com>
2026-04-13 12:01:08 +0530

    vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import { ThemeProvider } from './context/ThemeContext';
 import App from './App';
 import './index.css';
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <ThemeProvider>
         <App />
+        <Analytics />
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
